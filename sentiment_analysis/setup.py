42e171cff61ccf2fb3a3d501c879a65d5a3868df
Christian Conchari <chrisconchari@gmail.com>
2024-02-02 12:04:44 -0400

    setup added for sentiment-analysis package

---
+++ sentiment_analysis/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sentiment_analysis',
+    version='0.1.0',
+    url='https://github.com/ChristianConchari/multi_domain_sentiment_classifier',
+    author='Christian Conchari',
+    author_email='chrisconchari@gmail.com',
+    description='Sentiment analysis util functions for multiple domains',
+    packages=find_packages(),    
+    install_requires=[],
+)
